bea6eacc36764d67cb5adf7e06a9b8a2f49e1a82
DANYLO2204 <62219062+DANYLO2204@users.noreply.github.com>
2026-08-27 20:27:13 +0300

    Add installer publisher metadata

---
+++ installer/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("DGH Tools Revit 2023 Installer")]
+[assembly: AssemblyDescription("Online installer and updater for DGH Tools for Autodesk Revit 2023.")]
+[assembly: AssemblyCompany("DGH Tools")]
+[assembly: AssemblyProduct("DGH Tools for Autodesk Revit")]
+[assembly: AssemblyCopyright("Copyright © DGH Tools 2026")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
